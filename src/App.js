57f82e96fd8ec302c9aaaeba1b858c7ef91782db
petem14 <petem14@gmail.com>
2019-12-30 23:07:58 -0500

    add test code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_HOST = 'http://localhost:8000';
+
+let _csrfToken = null;
+
+async function getCsrfToken() {
+  if (_csrfToken === null) {
+    const response = await fetch(`${API_HOST}/csrf/`, {
+      credentials: 'include',
+    });
+    const data = await response.json();
+    _csrfToken = data.csrfToken;
+  }
+  return _csrfToken;
+}
+
+async function testRequest(method) {
+  const response = await fetch(`${API_HOST}/ping/`, {
+    method: method,
+    headers: (
+      method === 'POST'
+        ? {'X-CSRFToken': await getCsrfToken()}
+        : {}
+    ),
+    credentials: 'include',
+  });
+  const data = await response.json();
+  return data.result;
+}
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      testGet: 'KO',
+      testPost: 'KO',
+    };
+  }
+
+  async componentDidMount() {
+    this.setState({
+      testGet: await testRequest('GET'),
+      testPost: await testRequest('POST')
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Test GET request: {this.state.testGet}</p>
+        <p>Test POST request: {this.state.testPost}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
